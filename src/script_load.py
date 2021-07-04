93a68304a8ba8aa99df7bcbf52f98cf9486e4052
dailyMalotin <mar.michajlow@gmail.com>
2021-07-04 16:21:54 +0200

    added TODO

--- src/script_load.py
+++ src/script_load.py
@@ -14,6 +14,7 @@ import time
 #TODO: remove absolute paths
 #TODO: for requirements: chrome,... in rep -> relative path
 #TODO: distinguish between languages
+#TODO: get .gitignore working
 
 class script_load:
 
