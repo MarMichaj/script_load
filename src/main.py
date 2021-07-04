765c62b0beed242a897093c123cfd1ef861fb2ec
dailyMalotin <mar.michajlow@gmail.com>
2021-07-04 14:54:33 +0200

    deleted master branch

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Press the green button in the gutter to run the script.
+import autodownload
+a = autodownload.autodownload()
+a.set_base_url("https://www.happyscribe.com/public/lex-fridman-podcast-artificial-intelligence-ai")
+a.download_all()
+#a.get_file("https://www.happyscribe.com/public/lex-fridman-podcast-artificial-intelligence-ai/168-silvio-micali-cryptocurrency-blockchain-algorand-bitcoin-and-ethereum")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
